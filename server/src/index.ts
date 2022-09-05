9c6d8c31b373b52d21e71db9d6c73a51cd44ac00
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2022-09-05 13:24:04 -0700

    :sparkles: Apollo Server Express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,37 @@
-import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
 
+import { MikroORM } from "@mikro-orm/core";
 import mikroOrmConfig from "./mikro-orm.config";
+
+import { PORT } from "./constants";
+
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroOrmConfig);
     await orm.migrator.up();
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app, cors: { origin: "*" } });
+
+    app.use(helmet());
+    app.use(morgan("dev"));
+
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 };
 
 main();
-
-console.log("Hello!");
